fcc38ce78bbed890fbdb7a7df82c0dbfd2e5f899
1037HLY <hs202027323@gmail.com>
2026-08-01 22:40:30 +0800

    feat: 添加AppDatabase占位类

---
+++ app/src/main/java/com/geosurvey/toolbox/data/database/AppDatabase.kt
@@ -0,0 +1,7 @@
+package com.geosurvey.toolbox.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class AppDatabase : RoomDatabase()
