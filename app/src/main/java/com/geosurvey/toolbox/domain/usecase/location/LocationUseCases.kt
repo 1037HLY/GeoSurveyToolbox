595a89e767894daeaecb535424beb570fea6f3dd
1037HLY <hs202027323@gmail.com>
2026-08-01 22:42:32 +0800

    feat: 添加LocationUseCases占位类

---
+++ app/src/main/java/com/geosurvey/toolbox/domain/usecase/location/LocationUseCases.kt
@@ -0,0 +1,7 @@
+package com.geosurvey.toolbox.domain.usecase.location
+
+import com.geosurvey.toolbox.data.repository.LocationRepository
+
+class LocationUseCases(
+    private val locationRepository: LocationRepository
+)
