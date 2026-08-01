c7e09362f5468478e1bc01981dd74cd57e73ebb2
1037HLY <hs202027323@gmail.com>
2026-08-01 23:07:09 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/geosurvey/toolbox/data/database/AppDatabase.kt
+++ app/src/main/java/com/geosurvey/toolbox/data/database/AppDatabase.kt
@@ -2,6 +2,12 @@ package com.geosurvey.toolbox.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
-@Database(entities = [], version = 1)
+@Database(
+    entities = [],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase()
