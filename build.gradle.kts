6243f16be014c66d2ba66236652862936c2512d9
1037HLY <hs202027323@gmail.com>
2026-08-01 22:50:17 +0800

    fix: 恢复buildscript中的插件仓库配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 // Top-level build file
-plugins {
-    // 插件在app/build.gradle.kts中定义
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class) {
