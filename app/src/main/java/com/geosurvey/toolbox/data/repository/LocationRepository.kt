d253250154e87a98bfd4296ea496c7a340e478a1
1037HLY <hs202027323@gmail.com>
2026-08-02 15:17:59 +0800

    chore: 删除未使用的LocationRepository

--- app/src/main/java/com/geosurvey/toolbox/data/repository/LocationRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.geosurvey.toolbox.data.repository
-
-import android.content.Context
-import com.geosurvey.toolbox.data.database.AppDatabase
-
-class LocationRepository(
-    private val context: Context,
-    private val database: AppDatabase
-)
