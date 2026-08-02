b6d7803b366ee056cd21d53c804184a1bdc07f4e
1037HLY <hs202027323@gmail.com>
2026-08-02 15:43:27 +0800

    Delete app/src/main/java/com/geosurvey/toolbox/domain/usecase/location/LocationUseCases.kt

--- app/src/main/java/com/geosurvey/toolbox/domain/usecase/location/LocationUseCases.kt
+++
@@ -1,7 +0,0 @@
-package com.geosurvey.toolbox.domain.usecase.location
-
-import com.geosurvey.toolbox.data.repository.LocationRepository
-
-class LocationUseCases(
-    private val locationRepository: LocationRepository
-)
